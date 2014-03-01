518cde885760ef9be1a7f9aa4b95ee145f0fd5e8
MitulP91 <mitulp91@gmail.com>
2014-03-01 08:38:59 -0500

    first commit; basic rails add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FuckingOscars::Application.config.secret_key_base = '5193ac22c68f040c14900b27a99876c5239bdad438465a91a8901b3899bdbadcc3861f0aa67cfa72b650d8656f4fe8b226cec84c10ed2ad86b392792165f5e9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FuckingOscars::Application.config.session_store :cookie_store, key: '_fucking_oscars_session'
